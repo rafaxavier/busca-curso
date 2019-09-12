e392a652b97b83a64bc5ea6257dbb70d0c3d93eb
Rafael <rafaworksi@outlook.com>
2019-09-11 23:18:13 -0300

    v.1

---
+++ busca_curso.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Tempo de geração: 11/09/2019 às 23:16
+-- Versão do servidor: 5.7.27-0ubuntu0.18.04.1
+-- Versão do PHP: 7.2.19-0ubuntu0.18.04.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `busca_curso`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `COD_Usuario` int(10) UNSIGNED NOT NULL,
+  `perm_acesso` int(10) DEFAULT '0',
+  `cpf` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `path_avatar` varchar(1024) DEFAULT '_imgs\\avatar.png',
+  `informacoes` varchar(1024) DEFAULT 'Sem Informações'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Fazendo dump de dados para tabela `usuario`
+--
+
+INSERT INTO `usuario` (`COD_Usuario`, `perm_acesso`, `cpf`, `nome`, `login`, `senha`, `email`, `path_avatar`, `informacoes`) VALUES
+(1, 1, '000.000.000-00', 'administrador', 'admin', '12345', 'admin@admin.com', '_imgs\\avatar.png', 'Sem Informações');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
